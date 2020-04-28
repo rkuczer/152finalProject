1a52e74d25218f21bebaa825c468208e47ff0fe6
Ryan Kuczer <ryan-kooz@hotmail.com>
2020-04-28 15:54:28 -0400

    added main controller and main fxml file 4/28@3:54pm

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,7 +1,12 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -11,6 +16,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Parent root = null;
+        var loc = getClass().getResource("Main.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+        }
+        Scene windowContents = new Scene(root, 700,500);
+        primaryStage.setScene(windowContents);
+        primaryStage.setTitle("Main Menu");
+        primaryStage.show();
     }
 }
