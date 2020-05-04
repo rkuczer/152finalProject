cdeafba2df1e0149142e888443a8980962358a49
Stapes23 <x.staples19@gmail.com>
2020-05-04 11:40:23 -0400

    commit with alex, song finder, corrected, alex data handler, and alex controller, not fully done, having trouble with the user clicking the song on the list and gives similar songs to it

---
+++ src/main/java/bsu/comp152/alexController.java
@@ -0,0 +1,79 @@
+package bsu.comp152;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextInputDialog;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class alexController implements Initializable {
+    @FXML
+    private ListView<alexDataHandler.category> ListControl;
+    private alexDataHandler Model;
+    private String category;
+
+    public void loadData(){
+        var site = " https://searchly.asuarez.dev/docs/v1#tag/song";
+        var params = getQueryParameters();
+        var query = site+params;
+
+        Model = new alexDataHandler(query);
+        var songList = Model.getData();
+        ObservableList<alexDataHandler.category> dataToShow = FXCollections.observableArrayList(songList);
+        ListControl.setItems(dataToShow);
+    }
+
+    public String getQueryParameters(){
+        var category = getCategory();
+        return "/songs/"+category;
+    }
+
+    private String getCategory(){
+        TextInputDialog answer = new TextInputDialog("song finder");
+        answer.setContentText("Type the name of your artist.");
+        Optional<String> result = answer.showAndWait();
+        if (result.isPresent())
+            return result.get();
+        else
+            return "";
+    }
+
+    private String getCategory2(){
+        return category;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //loadData();
+        ListControl.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<alexDataHandler.category>() {
+                    @Override
+                    public void changed(ObservableValue<? extends alexDataHandler.category> observable,
+                                        alexDataHandler.category oldValue, alexDataHandler.category newValue) {
+                        var song = ListControl.getSelectionModel().getSelectedItem();
+                        Alert artistInfo = new Alert(Alert.AlertType.INFORMATION);
+                        artistInfo.setTitle("Joke and Category");
+                        artistInfo.setContentText("Joke: "+song.value);
+                        artistInfo.showAndWait();
+                    }
+                }
+        );
+    }
+
+    @FXML
+    public void selectMenuItem(javafx.event.ActionEvent actionEvent) {
+        var item =(MenuItem)actionEvent.getSource();
+        category = item.getText();
+    }
+
+
+}
+
---
+++ src/main/java/bsu/comp152/alexDataHandler.java
@@ -0,0 +1,61 @@
+package bsu.comp152;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+
+public class alexDataHandler {
+    private HttpClient dataGrabber;//
+    private String webLocation;
+
+    public alexDataHandler(String webLocation) {
+        dataGrabber = HttpClient.newHttpClient();
+        this.webLocation = webLocation;
+    }
+
+    public ArrayList<category> getData(){ //creates from API
+        var requestBuilder = HttpRequest.newBuilder();
+        var dataRequest = requestBuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response = null;
+        try {
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }catch(IOException e){
+            System.out.println("We could not connect to the network or the site is broken.");
+        }
+        catch (InterruptedException e){ //catches an interrupted connection to network
+            System.out.println("connection to the site is broken");
+        }
+        if (response == null){
+            System.out.println("Something went very wrong, ending the program");
+            System.exit(-1);
+        }
+        var usefulData = response.body();
+        var jsonInterpreter = new Gson();
+        var songData = jsonInterpreter.fromJson(usefulData, responseDataType.class); //creates data from a class responseDataType so the info can be parsed into an object.
+        System.out.println(songData.categories);
+        return songData.categories;
+
+    }
+
+    class responseDataType { // json information song finder  api response
+        ArrayList<category> categories;
+        String icon_url;
+        String id;
+        String url;
+        String value;
+    }
+
+    class category{ //stores data for the song
+        String value;
+        @Override
+        public String toString() {
+            return "Joke: " + value;
+        }
+    }
+}
