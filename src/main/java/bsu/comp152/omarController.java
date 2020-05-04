6702e223023e5d282771c61dfc9aaadd5c37e268
Omar1 <omarali.2000.16@gmail.com>
2020-05-04 16:41:08 -0400

    Added the omarHandler.java class and added some instance variable in the controller class

--- src/main/java/bsu/comp152/omarController.java
+++ src/main/java/bsu/comp152/omarController.java
@@ -1,4 +1,28 @@
 package bsu.comp152;
 
-public class omarController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class omarController implements Initializable {
+    @FXML
+    private ListView<ryanDataHandler.category> ListControl;
+    private ryanDataHandler Model;
+    private String jokeInput;
+
+
+
+
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
